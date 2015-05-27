1a3bd7a6cf35d3736196c71a6bd8af0689fb9366
Nuwaithu <nuwaithu@cloudsource.co.jp>
2015-05-27 10:51:00 +0630

    Initial Commit

---
+++ GithhubUseExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GithhubUseExample
+//
+//  Created by Nu Wai Thu on 5/27/15.
+//  Copyright (c) 2015 clousource. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GithhubUseExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GithhubUseExample
+//
+//  Created by Nu Wai Thu on 5/27/15.
+//  Copyright (c) 2015 clousource. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
